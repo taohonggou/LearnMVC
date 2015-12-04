79222affe89dea9686803e301009fb6404a322bf
taohonggou <cl889521@163.com>
2015-12-04 12:26:21 +0800

    刚刚新建好项目

---
+++ learnmvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnMVC.Startup))]
+namespace LearnMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
